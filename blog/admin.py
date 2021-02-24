bd209953324d53c98223343e3e93d4ce776302d0
yogata733 <y.ogata.733@gmail.com>
2021-02-24 14:36:16 +0000

    first add

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from . import models
 
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.admin import AdminSite
+
 @admin.register(models.Category)
 class CategoryAdmin(admin.ModelAdmin):
     pass
@@ -14,3 +17,21 @@ class TagAdmin(admin.ModelAdmin):
 @admin.register(models.Post)
 class PostAdmin(admin.ModelAdmin):
     pass
+
+
+class BlogAdminSite(AdminSite):
+    site_header = 'マイページ'
+    site_title = 'マイページ'
+    index_title = 'ホーム'
+    site_url = None
+    login_form = AuthenticationForm
+
+    def has_permission(self, request):
+        return request.user.is_active
+
+
+mypage_site = BlogAdminSite(name="mypage")
+
+mypage_site.register(models.Post)
+mypage_site.register(models.Tag)
+mypage_site.register(models.Category)
